a147452240e55e1cc93eea41bbce4ff17a7f066e
Damien Dub <damien.dub@gmail.com>
2023-02-19 11:48:58 +0100

    Back to original wasm path

--- www/index.js
+++ www/index.js
@@ -1,12 +1,12 @@
-import * as wasm from "./pkg/chicken_rust.js";
+import * as wasm from "chicken-rust";
 
 console.log("SHA-1: " + wasm.sha1("test"));
 
 try {
-
+    
     let base64_encoded = wasm.base64_encode("test")
     console.log("Base 64 encoded string: " + base64_encoded);
-
+    
     let base64_decoded = wasm.base64_decode(base64_encoded)
     console.log("Base 64 decoded string: " + base64_decoded);
 }
@@ -15,10 +15,10 @@ catch (error) {
 }
 
 try {
-
+    
     let url_encoded = wasm.url_encode("https://chickenrust.com?var= Go go go");
     console.log("URL encoded: " + url_encoded);
-
+    
     let url_decoded = wasm.url_decode(url_encoded);
     console.log("URL decoded: " + url_decoded);
 }
